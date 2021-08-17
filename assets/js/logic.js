349a546ee44ff6b3d2242ec5b3af198444bffb8c
Tino Carrier <tino.tyreecarrier@yahoo.com>
2021-08-17 00:15:35 -0500

    Added randomly generated first question along with 4 choices

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,6 +3,9 @@ var next = document.getElementById("next");
 var printedCountdown = document.getElementById("timer");
 var questionPrompt = document.getElementById("question-prompt");
 var answerA = document.getElementById("answer-A");
+var answerB = document.getElementById("answer-B");
+var answerC = document.getElementById("answer-C");
+var answerD = document.getElementById("answer-D");
 
 // Create a confirm box and store it in variable
 var confirmation = confirm("You have 1min to complete all 5 questions, timer starts when you click 'OK', GoodLuck!")
@@ -25,11 +28,13 @@ var questions = [
 ]
     // Populate first question
 if(confirmation) {
-    questionPrompt.textContent = questions[Math.floor(Math.random()*3)];
+    questionPrompt.textContent = questions[Math.floor(Math.random()*5)];
 }
-// Populate the answers for first question
-switch(questions[0]) {
-    case "What is the output of the following code?": 
-    break;
-    default: console.log("idk");
+    // Populate the answers for first question
+if (questionPrompt.textContent === questions[0]) {
+    answerA.textContent = "This is answer A for prompt 1";
+    answerB.textContent = "This is answer B for prompt 1";
+    answerC.textContent = "This is answer C for prompt 1";
+    answerD.textContent = "This is answer D for prompt 1"
 }
+// Populate answers for second question
